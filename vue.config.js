3810badf7b98617c6f9d218a3dedd53fa98909f2
s836064858 <836064858@qq.com>
2022-07-18 09:51:05 +0800

    fix: 添加图标

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,19 @@ module.exports = defineConfig({
       nodeIntegration: true,
       builderOptions: {
         productName: '常用短语',
-        copyright: 'Copyright © 2022 raozi'
+        copyright: 'Copyright © 2022 raozi',
+        mac: {
+          icon: './src/assets/icon_512x512.png' //图标路径
+        },
+        win: {
+          icon: './public/favicon.ico'
+        },
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true,
+          installerIcon: './public/favicon.ico', // 安装图标
+          createDesktopShortcut: true // 创建桌面图标
+        }
       }
     }
   }
